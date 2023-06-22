e7a6e35087c7920b02a0c1370845a6e7df8b8fd1
Dmitry G <sdprecovery@gmail.com>
2023-06-22 16:45:22 +0300

    Add admins for Quiz models
    Migration:
        - Change Meta options on quiz
        - Change Meta options on quizanswer
        - Change Meta options on quizcategory
        - Change Meta options on quizquestion
        - Alter field category on quiz

--- qz/quiz/admin.py
+++ qz/quiz/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quiz, QuizAnswer, QuizCategory, QuizQuestion
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ("title", "category")
+
+
+@admin.register(QuizCategory)
+class QuizCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(QuizQuestion)
+class QuizQuestionAdmin(admin.ModelAdmin):
+    list_display = ("quiz", "question")
+
+
+@admin.register(QuizAnswer)
+class QuizAnswerAdmin(admin.ModelAdmin):
+    list_display = ("question", "answer")
+    list_filter = ("question",)
+    search_fields = ("question__question",)
+    ordering = ("question",)
--- qz/quiz/models.py
+++ qz/quiz/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
 
+class QuizCategory(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name_plural = "Quiz categories"
+
+
 class Quiz(models.Model):
+    category = models.ForeignKey(QuizCategory, related_name="quizzes", on_delete=models.CASCADE)
+
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return str(self.title)
 
-class QuizCategory(models.Model):
-    title = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = "Quizzes"
+        ordering = ["category__id"]
 
 
 class QuizQuestion(models.Model):
@@ -15,9 +30,23 @@ class QuizQuestion(models.Model):
 
     question = models.TextField()
 
+    def __str__(self):
+        return str(self.question)
+
+    class Meta:
+        verbose_name_plural = "Quiz questions"
+        ordering = ["quiz__id"]
+
 
 class QuizAnswer(models.Model):
     question = models.ForeignKey(QuizQuestion, related_name="answers", on_delete=models.CASCADE)
 
     answer = models.TextField()
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.answer)
+
+    class Meta:
+        verbose_name_plural = "Quiz answers"
+        ordering = ["question__id"]
