c262df2183292a836b5dc686ab4420fbd09b813d
Dmitry G <sdprecovery@gmail.com>
2023-06-22 14:14:11 +0300

    Init

---
+++ qz/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from qz.users.models import User
+from qz.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
