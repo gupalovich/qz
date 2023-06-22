3166d8000ad0e91193b45c864d6714a1c0a48f01
Dmitry G <sdprecovery@gmail.com>
2023-06-22 14:36:46 +0300

    Update gulp dependencies - Install htmx

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,8 @@ function pathsConfig(appName) {
     vendorsJs: [
       `${vendorsRoot}/@popperjs/core/dist/umd/popper.js`,
       `${vendorsRoot}/bootstrap/dist/js/bootstrap.js`,
+      `${vendorsRoot}/htmx.org/dist/htmx.js`,
+      `${vendorsRoot}/htmx.org/dist/ext/debug.js`,
     ],
     app: this.app,
     templates: `${this.app}/templates`,
@@ -56,16 +58,16 @@ function styles() {
   ];
 
   const minifyCss = [
-    cssnano({ preset: 'default' }), // minify result
+    cssnano({ preset: 'default' }) // minify result
   ];
 
   return src(`${paths.sass}/project.scss`)
-    .pipe(
-      sass({
-        importer: tildeImporter,
-        includePaths: [paths.sass],
-      }).on('error', sass.logError),
-    )
+    .pipe(sass({
+      importer: tildeImporter,
+      includePaths: [
+        paths.sass
+      ]
+    }).on('error', sass.logError))
     .pipe(plumber()) // Checks for errors
     .pipe(postcss(processCss))
     .pipe(dest(paths.css))
@@ -112,41 +114,49 @@ function runServer(cb) {
 // Browser sync server for live reload
 function initBrowserSync() {
   browserSync.init(
-    [`${paths.css}/*.css`, `${paths.js}/*.js`, `${paths.templates}/*.html`],
-    {
-      // https://www.browsersync.io/docs/options/#option-open
-      // Disable as it doesn't work from inside a container
-      open: false,
-      // https://www.browsersync.io/docs/options/#option-proxy
-      proxy: {
-        target: '127.0.0.1:8000',
-        proxyReq: [
-          function (proxyReq, req) {
-            // Assign proxy 'host' header same as current request at Browsersync server
-            proxyReq.setHeader('Host', req.headers.host);
-          },
-        ],
-      },
-    },
+    [
+      `${paths.css}/*.css`,
+      `${paths.js}/*.js`,
+      `${paths.templates}/*.html`
+    ], {
+    // Prevent opening browser
+    open: false,
+    // https://www.browsersync.io/docs/options/#option-proxy
+    proxy: {
+      target: '127.0.0.1:8000',
+      proxyReq: [
+        function (proxyReq, req) {
+          // Assign proxy "host" header same as current request at Browsersync server
+          proxyReq.setHeader('Host', req.headers.host);
+        }
+      ]
+    }
+  }
   );
 }
 
 // Watch
 function watchPaths() {
   watch(`${paths.sass}/*.scss`, styles);
-  watch(`${paths.templates}/**/*.html`).on('change', reload);
-  watch([`${paths.js}/*.js`, `!${paths.js}/*.min.js`], scripts).on(
-    'change',
-    reload,
-  );
+  watch(`${paths.templates}/**/*.html`).on("change", reload);
+  watch([`${paths.js}/*.js`, `!${paths.js}/*.min.js`], scripts).on("change", reload);
 }
 
 // Generate all assets
-const generateAssets = parallel(styles, scripts, vendorScripts, imgCompression);
+const generateAssets = parallel(
+  styles,
+  scripts,
+  vendorScripts,
+  imgCompression
+);
 
 // Set up dev environment
-const dev = parallel(runServer, initBrowserSync, watchPaths);
+const dev = parallel(
+  runServer,
+  initBrowserSync,
+  watchPaths
+);
 
 exports.default = series(generateAssets, dev);
-exports['generate-assets'] = generateAssets;
-exports['dev'] = dev;
+exports["generate-assets"] = generateAssets;
+exports["dev"] = dev;
