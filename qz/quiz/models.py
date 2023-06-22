a4af6562e23faec4ef1340d60e2a7ea5ac65ef05
Dmitry G <sdprecovery@gmail.com>
2023-06-22 16:28:37 +0300

    Create model Quiz, QuizCategory, QuizQuestion, QuizAnswer

--- qz/quiz/models.py
+++ qz/quiz/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+class QuizCategory(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class QuizQuestion(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name="questions", on_delete=models.CASCADE)
+
+    question = models.TextField()
+
+
+class QuizAnswer(models.Model):
+    question = models.ForeignKey(QuizQuestion, related_name="answers", on_delete=models.CASCADE)
+
+    answer = models.TextField()
+    is_correct = models.BooleanField(default=False)
